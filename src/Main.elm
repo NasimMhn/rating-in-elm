76dfcf9eadcf2ea936ebde3b30b96658933e5b87
nasimmhn <nasim.mahzoun@gmail.com>
2020-03-07 16:49:07 +0100

    buttons now changing on click

--- src/Main.elm
+++ src/Main.elm
@@ -1,43 +1,40 @@
 module Main exposing (main)
 
 import Browser
-import Html exposing (Html, button, div, h1, text)
+-- import Html exposing (Html, button, div, h1, text)
+-- import Html.Attributes exposing (class, id)
+-- import Html.Events exposing (onClick)
+import Html exposing (Html, h1, div, text, button)
 import Html.Attributes exposing (class)
 import Html.Events exposing (onClick)
 
-
 type alias Model =
-    { count : Int }
-
+    { horse : String }
 
 initialModel : Model
 initialModel =
-    { count = 0 }
-
+    { horse = "♘" }
 
 type Msg
-    = Increment
-    | Decrement
+    = Rating
 
 
 update : Msg -> Model -> Model
 update msg model =
     case msg of
-        Increment ->
-            { model | count = model.count + 1 }
-
-        Decrement ->
-            { model | count = model.count - 1 }
-
+      Rating -> 
+        { model | horse = "♞"}
 
 view : Model -> Html Msg
 view model =
     div [ class "main-div" ]
-        [ h1 [ class "header" ] [ text "♞ Rate it! ♘" ]
-        , button [ onClick Increment ] [ text "+1" ]
-        , div [] [ text <| String.fromInt model.count ]
-        , button [ onClick Decrement ] [ text "-1" ]
-        ]
+      [ 
+        h1 [] [ text "♞ Rate it! ♘"]
+      , h1 [ class "header" ] [ text "NEW RATING" ]
+      , button [ class "horse", onClick Rating] [ text <| model.horse ]
+      , button [ class "horse", onClick Rating] [ text <| model.horse ]
+      ]
+      
 
 
 main : Program () Model Msg
