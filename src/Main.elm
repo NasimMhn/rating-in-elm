6c17d494008f7a7a2d3d6bb70aaa1a9601c31a12
nasimmhn <nasim.mahzoun@gmail.com>
2020-03-07 14:33:34 +0100

    first set up

---
+++ src/Main.elm
@@ -0,0 +1,49 @@
+module Main exposing (main)
+
+import Browser
+import Html exposing (Html, button, div, h1, text)
+import Html.Attributes exposing (class)
+import Html.Events exposing (onClick)
+
+
+type alias Model =
+    { count : Int }
+
+
+initialModel : Model
+initialModel =
+    { count = 0 }
+
+
+type Msg
+    = Increment
+    | Decrement
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Increment ->
+            { model | count = model.count + 1 }
+
+        Decrement ->
+            { model | count = model.count - 1 }
+
+
+view : Model -> Html Msg
+view model =
+    div [ class "main-div" ]
+        [ h1 [ class "header" ] [ text "♞ Rate it! ♘" ]
+        , button [ onClick Increment ] [ text "+1" ]
+        , div [] [ text <| String.fromInt model.count ]
+        , button [ onClick Decrement ] [ text "-1" ]
+        ]
+
+
+main : Program () Model Msg
+main =
+    Browser.sandbox
+        { init = initialModel
+        , view = view
+        , update = update
+        }
